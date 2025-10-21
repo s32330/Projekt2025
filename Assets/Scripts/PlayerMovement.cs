e94d3abd982756d7fc9f7fd0def21dd616be46ed
s32330 <s32330@pjwstk.edu.pl>
2025-10-21 18:54:38 +0200

    2 zmiana 21.10.2025

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 1000;
+    public float moveSpeed = 500;
+    public float runSpeed = 800;
     public float moveInput = 0;
     public float jumpForce = 300;
     public bool isJump = false;
+    public bool isRun = false;
 
     public Rigidbody2D rb;
     public SpriteRenderer sprite;
@@ -31,13 +33,35 @@ public class PlayerMovement : MonoBehaviour
             isJump = true; 
         }
 
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isRun = true;
+        }
+
 
     }
 
     //FixedUpdate zalezy od ilosci sekund (co 0.02 sek) a nie klatek
     private void FixedUpdate()
     {
+
+        if (moveInput > 0)
+        {
+            sprite.flipX = false;
+        }
+        else if (moveInput < 0)
+        {
+            sprite.flipX = true;
+        }
+
         rb.velocity = new Vector2(moveInput * moveSpeed * Time.deltaTime, rb.velocity.y);
+
+        if (isRun)
+        {
+            rb.velocity = new Vector2(moveInput * runSpeed * Time.deltaTime, rb.velocity.y);
+            isRun = false;
+        }
+        
 
         if (isJump && groundChecker.isGrounded)
         {
